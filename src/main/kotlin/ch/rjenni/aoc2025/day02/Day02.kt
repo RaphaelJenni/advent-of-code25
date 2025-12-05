20593146003f57bef58a90fd92ae2ce39f0bfe54
Raphael Jenni <info@rjenni.ch>
2025-12-05 13:30:37 +0100

    Day 02 Part 2

--- src/main/kotlin/ch/rjenni/aoc2025/day02/Day02.kt
+++ src/main/kotlin/ch/rjenni/aoc2025/day02/Day02.kt
@@ -3,7 +3,7 @@ package ch.rjenni.aoc2025.day02
 import ch.rjenni.aoc2025.utils.InputReader
 
 class Day02 {
-    fun solve(input: String): Long {
+    fun solvePart1(input: String): Long {
         // Remove newlines just in case, though the problem says single line.
         val cleanedInput = input.replace("\n", "").replace("\r", "")
         val ranges = cleanedInput.split(",")
@@ -16,7 +16,7 @@ class Day02 {
             val end = parts[1].toLong()
 
             for (i in start..end) {
-                if (isInvalid(i)) {
+                if (isInvalidPart1(i)) {
                     totalSum += i
                 }
             }
@@ -24,20 +24,61 @@ class Day02 {
         return totalSum
     }
 
-    private fun isInvalid(number: Long): Boolean {
+    fun solvePart2(input: String): Long {
+        val cleanedInput = input.replace("\n", "").replace("\r", "")
+        val ranges = cleanedInput.split(",")
+        var totalSum = 0L
+
+        for (range in ranges) {
+            if (range.isBlank()) continue
+            val parts = range.split("-")
+            val start = parts[0].toLong()
+            val end = parts[1].toLong()
+
+            for (i in start..end) {
+                if (isInvalidPart2(i)) {
+                    totalSum += i
+                }
+            }
+        }
+        return totalSum
+    }
+
+    private fun isInvalidPart1(number: Long): Boolean {
         val s = number.toString()
         if (s.length % 2 != 0) {
             return false
         }
         val mid = s.length / 2
-        val firstHalf = s.take(mid)
+        val firstHalf = s.substring(0, mid)
         val secondHalf = s.substring(mid)
         return firstHalf == secondHalf
     }
 
+    private fun isInvalidPart2(number: Long): Boolean {
+        val sequence = number.toString()
+        val len = sequence.length
+        if (len < 2) return false // Cannot repeat at least twice if length is 1
+
+        // Try all possible lengths for the repeating sequence
+        for (subSeqLen in 1..len / 2) {
+            // The sub sequence length must be a divisor of len
+            if (len % subSeqLen == 0) {
+                val pattern = sequence.take(subSeqLen)
+                val numberOfRepetitions = len / subSeqLen
+                val constructedSequence = pattern.repeat(numberOfRepetitions)
+                if (constructedSequence == sequence) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
     fun run() {
         val input = InputReader.readInputAsString("02").trim()
-        println("Day 02 Result: ${solve(input)}")
+        println("Day 02 Part 1: ${solvePart1(input)}")
+        println("Day 02 Part 2: ${solvePart2(input)}")
     }
 }
 
--- src/test/kotlin/ch/rjenni/aoc2025/day02/Day02Test.kt
+++ src/test/kotlin/ch/rjenni/aoc2025/day02/Day02Test.kt
@@ -6,17 +6,31 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class Day02Test : FunSpec({
-    test("Day 02 Example") {
+    test("Day 02 Part 1 Example") {
         val input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
         val day02 = Day02()
-        day02.solve(input) shouldBe 1227775554L
+        day02.solvePart1(input) shouldBe 1227775554L
     }
 
-    test("Day 02 Real Input") {
-        val input = InputReader.readInput("02")
+    test("Day 02 Part 2 Example") {
+        val input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
         val day02 = Day02()
-        val result = day02.solve(input.first())
+        day02.solvePart2(input) shouldBe 4174379265L
+    }
 
-        result shouldBe 19219508902
+    test("Day 02 Part 1 Real Input") {
+        val input = InputReader.readInputAsString("02").trim()
+        val day02 = Day02()
+        val result = day02.solvePart1(input)
+        
+        result shouldBe 19219508902L
+    }
+
+    test("Day 02 Part 2 Real Input") {
+        val input = InputReader.readInputAsString("02").trim()
+        val day02 = Day02()
+        val result = day02.solvePart2(input)
+
+        result shouldBe 27180728081L
     }
 })
